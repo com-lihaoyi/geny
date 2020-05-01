054523eb517170425792ea7659911a0aec55e0fd
Anatolii Kmetiuk <anatoliykmetyuk@gmail.com>
2020-05-01 18:38:44 +0200

    Make tests pass under Dotty

--- geny/test/src/geny/TestGenerator.scala
+++ geny/test/src/geny/TestGenerator.scala
@@ -21,62 +21,62 @@ object TestGenerator extends TestSuite{
         assert(toSeq == expected)
       }
       test("toSeq"){
-        check(Generator.from(0 until 10), 0 until 10)
+        check(Generator.from[IndexedSeq, Int](0 until 10), 0 until 10)
       }
 
       test("find"){
-        assert(Generator.from(0 until 10).find(_ % 5 == 4) == Some(4))
-        assert(Generator.from(0 until 10).find(_ % 100 == 40) == None)
+        assert(Generator.from[IndexedSeq, Int](0 until 10).find(_ % 5 == 4) == Some(4))
+        assert(Generator.from[IndexedSeq, Int](0 until 10).find(_ % 100 == 40) == None)
       }
       test("exists"){
-        assert(Generator.from(0 until 10).exists(_ == 4) == true)
-        assert(Generator.from(0 until 10).exists(_ == 40) == false)
+        assert(Generator.from[IndexedSeq, Int](0 until 10).exists(_ == 4) == true)
+        assert(Generator.from[IndexedSeq, Int](0 until 10).exists(_ == 40) == false)
       }
       test("contains"){
-        assert(Generator.from(0 until 10).contains(4) == true)
-        assert(Generator.from(0 until 10).contains(40) == false)
+        assert(Generator.from[IndexedSeq, Int](0 until 10).contains(4) == true)
+        assert(Generator.from[IndexedSeq, Int](0 until 10).contains(40) == false)
       }
       test("forAll"){
-        assert(Generator.from(0 until 10).forall(_  < 100) == true)
-        assert(Generator.from(0 until 10).forall(_ < 5) == false)
+        assert(Generator.from[IndexedSeq, Int](0 until 10).forall(_  < 100) == true)
+        assert(Generator.from[IndexedSeq, Int](0 until 10).forall(_ < 5) == false)
       }
       test("count"){
-        assert(Generator.from(0 until 10).count(_  < 100) == 10)
-        assert(Generator.from(0 until 10).count(_  > 100) == 0)
-        assert(Generator.from(0 until 10).count(_ < 5) == 5)
-        assert(Generator.from(0 until 10).count() == 10)
+        assert(Generator.from[IndexedSeq, Int](0 until 10).count(_  < 100) == 10)
+        assert(Generator.from[IndexedSeq, Int](0 until 10).count(_  > 100) == 0)
+        assert(Generator.from[IndexedSeq, Int](0 until 10).count(_ < 5) == 5)
+        assert(Generator.from[IndexedSeq, Int](0 until 10).count() == 10)
       }
 
       test("reduceLeft"){
-        assert(Generator.from(0 until 10).reduceLeft(_ + _) == 45)
+        assert(Generator.from[IndexedSeq, Int](0 until 10).reduceLeft(_ + _) == 45)
         intercept[UnsupportedOperationException](
-          Generator.from(0 until 0).reduceLeft(_ + _)
+          Generator.from[IndexedSeq, Int](0 until 0).reduceLeft(_ + _)
         )
       }
       test("foldLeft"){
-        assert(Generator.from(0 until 10).foldLeft(0)(_ + _) == 45)
-        assert(Generator.from(0 until 0).foldLeft(0)(_ + _) == 0)
+        assert(Generator.from[IndexedSeq, Int](0 until 10).foldLeft(0)(_ + _) == 45)
+        assert(Generator.from[IndexedSeq, Int](0 until 0).foldLeft(0)(_ + _) == 0)
       }
 
 
       test("concat"){
         check(
-          Generator.from(0 until 10) ++ Generator.from(0 until 10),
+          Generator.from[IndexedSeq, Int](0 until 10) ++ Generator.from[IndexedSeq, Int](0 until 10),
           (0 until 10) ++ (0 until 10)
         )
         check(
-          Generator.from(0 until 10) ++ Generator.from(10 until 20),
+          Generator.from[IndexedSeq, Int](0 until 10) ++ Generator.from[IndexedSeq, Int](10 until 20),
           0 until 20
         )
       }
       test("filter"){
-        check(Generator.from(0 until 10).filter(_ > 5), 6 until 10)
-        check(Generator.from(0 until 10).withFilter(_ > 5), 6 until 10)
+        check(Generator.from[IndexedSeq, Int](0 until 10).filter(_ > 5), 6 until 10)
+        check(Generator.from[IndexedSeq, Int](0 until 10).withFilter(_ > 5), 6 until 10)
       }
       test("map"){
-        check(Generator.from(0 until 10).map(_ + 1), 1 until 11)
+        check(Generator.from[IndexedSeq, Int](0 until 10).map(_ + 1), 1 until 11)
         check(
-          Generator.from(0 until 10).map(i => i.toString * i),
+          Generator.from[IndexedSeq, Int](0 until 10).map(i => i.toString * i),
           Seq(
             "",
             "1",
@@ -104,39 +104,39 @@ object TestGenerator extends TestSuite{
           1, 2, 8,
           1, 2, 9
         )
-        check(Generator.from(0 until 10).flatMap(x => Seq(1, 2, x)), expected)
+        check(Generator.from[IndexedSeq, Int](0 until 10).flatMap(x => Seq(1, 2, x)), expected)
         check(
-          Generator.from(0 until 10).flatMap(x => Generator.from(Seq(1, 2, x))),
+          Generator.from[IndexedSeq, Int](0 until 10).flatMap(x => Generator.from(Seq(1, 2, x))),
           expected
         )
       }
       test("collect"){
         check(
-          Generator.from(0 until 10).collect{case k if k % 2 == 0 => k * k},
+          Generator.from[IndexedSeq, Int](0 until 10).collect{case k if k % 2 == 0 => k * k},
           Seq(0, 4, 16, 36, 64)
         )
       }
       test("collectFirst"){
-        Generator.from(0 until 10).collectFirst{case k if k > 5 => k * k} ==> Some(36)
-        Generator.from(0 until 10).collectFirst{case k if k > 15 => k * k} ==> None
+        Generator.from[IndexedSeq, Int](0 until 10).collectFirst{case k if k > 5 => k * k} ==> Some(36)
+        Generator.from[IndexedSeq, Int](0 until 10).collectFirst{case k if k > 15 => k * k} ==> None
       }
       test("slice"){
-        check(Generator.from(0 until 10).slice(3, 7), 3 until 7)
-        check(Generator.from(0 until 10).take(3), 0 until 3)
-        check(Generator.from(0 until 10).take(0), 0 until 0)
-        check(Generator.from(0 until 10).take(999), 0 until 10)
-        check(Generator.from(0 until 10).drop(3), 3 until 10)
-        check(Generator.from(0 until 10).drop(-1), 0 until 10)
+        check(Generator.from[IndexedSeq, Int](0 until 10).slice(3, 7), 3 until 7)
+        check(Generator.from[IndexedSeq, Int](0 until 10).take(3), 0 until 3)
+        check(Generator.from[IndexedSeq, Int](0 until 10).take(0), 0 until 0)
+        check(Generator.from[IndexedSeq, Int](0 until 10).take(999), 0 until 10)
+        check(Generator.from[IndexedSeq, Int](0 until 10).drop(3), 3 until 10)
+        check(Generator.from[IndexedSeq, Int](0 until 10).drop(-1), 0 until 10)
       }
       test("takeWhile"){
-        check(Generator.from(0 until 10).takeWhile(_ < 5), 0 until 5)
+        check(Generator.from[IndexedSeq, Int](0 until 10).takeWhile(_ < 5), 0 until 5)
       }
       test("dropWhile"){
-        check(Generator.from(0 until 10).dropWhile(_ < 5), 5 until 10)
+        check(Generator.from[IndexedSeq, Int](0 until 10).dropWhile(_ < 5), 5 until 10)
       }
       test("zipWithIndex"){
         check(
-          Generator.from(5 until 10).zipWithIndex,
+          Generator.from[IndexedSeq, Int](5 until 10).zipWithIndex,
           Seq(
             5 -> 0,
             6 -> 1,
@@ -148,7 +148,7 @@ object TestGenerator extends TestSuite{
       }
       test("zip"){
         test("simple") - check(
-          Generator.from(0 until 5).zip(Seq('a', 'b', 'c', 'd', 'e')),
+          Generator.from[IndexedSeq, Int](0 until 5).zip(Seq('a', 'b', 'c', 'd', 'e')),
           Seq(
             0 -> 'a',
             1 -> 'b',
@@ -158,7 +158,7 @@ object TestGenerator extends TestSuite{
           )
         )
         test("truncateIfGenLonger") - check(
-          Generator.from(0 until 99).zip(Seq('a', 'b', 'c', 'd', 'e')),
+          Generator.from[IndexedSeq, Int](0 until 99).zip(Seq('a', 'b', 'c', 'd', 'e')),
           Seq(
             0 -> 'a',
             1 -> 'b',
@@ -168,7 +168,7 @@ object TestGenerator extends TestSuite{
           )
         )
         test("truncateIfIterableLonger") - check(
-          Generator.from(0 until 5).zip(Seq('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i')),
+          Generator.from[IndexedSeq, Int](0 until 5).zip(Seq('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i')),
           Seq(
             0 -> 'a',
             1 -> 'b',
@@ -180,44 +180,44 @@ object TestGenerator extends TestSuite{
       }
       test("head"){
         assert(
-          Generator.from(0 until 10).head == 0,
-          Generator.from(5 until 10).head == 5
+          Generator.from[IndexedSeq, Int](0 until 10).head == 0,
+          Generator.from[IndexedSeq, Int](5 until 10).head == 5
         )
       }
       test("headOption"){
         assert(
-          Generator.from(0 until 10).headOption == Some(0),
-          Generator.from(5 until 10).headOption == Some(5),
-          Generator.from(0 until 0).headOption == None
+          Generator.from[IndexedSeq, Int](0 until 10).headOption == Some(0),
+          Generator.from[IndexedSeq, Int](5 until 10).headOption == Some(5),
+          Generator.from[IndexedSeq, Int](0 until 0).headOption == None
         )
       }
       test("conversions"){
         assert(
-          Generator.from(0 until 10).toSeq == (0 until 10),
-          Generator.from(0 until 10).toVector == (0 until 10),
-          Generator.from(0 until 10).toArray.toSeq == (0 until 10),
-          Generator.from(0 until 10).toVector == (0 until 10),
-          Generator.from(0 until 10).toList == (0 until 10),
-          Generator.from(0 until 10).toSet == (0 until 10).toSet
+          Generator.from[IndexedSeq, Int](0 until 10).toSeq == (0 until 10),
+          Generator.from[IndexedSeq, Int](0 until 10).toVector == (0 until 10),
+          Generator.from[IndexedSeq, Int](0 until 10).toArray.toSeq == (0 until 10),
+          Generator.from[IndexedSeq, Int](0 until 10).toVector == (0 until 10),
+          Generator.from[IndexedSeq, Int](0 until 10).toList == (0 until 10),
+          Generator.from[IndexedSeq, Int](0 until 10).toSet == (0 until 10).toSet
         )
       }
       test("mkString"){
         assert(
-          Generator.from(0 until 10).mkString == "0123456789",
-          Generator.from(0 until 10).mkString(" ") == "0 1 2 3 4 5 6 7 8 9",
-          Generator.from(0 until 10).mkString("[", ", ", "]") == "[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]"
+          Generator.from[IndexedSeq, Int](0 until 10).mkString == "0123456789",
+          Generator.from[IndexedSeq, Int](0 until 10).mkString(" ") == "0 1 2 3 4 5 6 7 8 9",
+          Generator.from[IndexedSeq, Int](0 until 10).mkString("[", ", ", "]") == "[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]"
         )
       }
       test("aggregates"){
         assert(
-          Generator.from(0 until 10).min == 0,
-          Generator.from(0 until 10).max == 9,
-          Generator.from(0 until 10).maxBy(x => math.abs(3 - x)) == 9,
-          Generator.from(0 until 10).minBy(x => math.abs(3 - x)) == 3,
-          Generator.from(0 until 10).sum == 45,
-          Generator.from(1 until 10).product == 362880,
-          Generator.from(0 until 10).reduce(_ + _) == 45,
-          Generator.from(0 until 10).fold(0)(_ + _) == 45
+          Generator.from[IndexedSeq, Int](0 until 10).min == 0,
+          Generator.from[IndexedSeq, Int](0 until 10).max == 9,
+          Generator.from[IndexedSeq, Int](0 until 10).maxBy(x => math.abs(3 - x)) == 9,
+          Generator.from[IndexedSeq, Int](0 until 10).minBy(x => math.abs(3 - x)) == 3,
+          Generator.from[IndexedSeq, Int](0 until 10).sum == 45,
+          Generator.from[IndexedSeq, Int](1 until 10).product == 362880,
+          Generator.from[IndexedSeq, Int](0 until 10).reduce(_ + _) == 45,
+          Generator.from[IndexedSeq, Int](0 until 10).fold(0)(_ + _) == 45
         )
       }
       test("creation"){
@@ -269,7 +269,7 @@ object TestGenerator extends TestSuite{
     }
     test("laziness"){
       var count = 0
-      val taken = Generator.from(0 until 10).map{ x => count += 1; x + 1}.take(3)
+      val taken = Generator.from[IndexedSeq, Int](0 until 10).map{ x => count += 1; x + 1}.take(3)
       // Before evaluation, nothing has happened!
       assert(count == 0)
       val seqed = taken.toSeq
@@ -285,7 +285,7 @@ object TestGenerator extends TestSuite{
     test("combinations"){
       def check[V](mkGen: Generator[Int] => Generator[V], mkSeq: Seq[Int] => Seq[V]) = {
         val seq = 0 until 10
-        val gen = Generator.from(seq)
+        val gen = Generator.from[IndexedSeq, Int](seq)
         val seq1 = mkGen(gen).toSeq
         val seq2 = mkSeq(seq)
         assert(seq1 == seq2)
@@ -323,7 +323,7 @@ object TestGenerator extends TestSuite{
       }
       test{
         check(
-          _.filter(_ % 3 == 0).flatMap(0 until _).drop(2).take(9).flatMap(0 until _).slice(2, 8),
+          _.filter(_ % 3 == 0).flatMap(x => Generator.from[IndexedSeq, Int](0 until x)).drop(2).take(9).flatMap(x => Generator.from[IndexedSeq, Int](0 until x)).slice(2, 8),
           _.filter(_ % 3 == 0).flatMap(0 until _).drop(2).take(9).flatMap(0 until _).slice(2, 8)
         )
       }
