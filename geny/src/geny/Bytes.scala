e16cf453c1d3b2b29eb67b20c34c5ce331305d7d
Ondra Pelech <ondra.pelech@gmail.com>
2024-09-01 22:32:42 +0200

    Improve Bytes: add hashCode and better toString

--- geny/src/geny/Bytes.scala
+++ geny/src/geny/Bytes.scala
@@ -1,5 +1,9 @@
 package geny
 
+import java.math.BigInteger
+import java.nio.ByteBuffer
+import java.nio.charset.{CodingErrorAction, StandardCharsets}
+
 /**
  * Trivial wrapper around `Array[Byte]` with sane equality and useful toString
  */
@@ -8,5 +12,15 @@ class Bytes(val array: Array[Byte]){
     case otherBytes: Bytes => java.util.Arrays.equals(array, otherBytes.array)
     case _ => false
   }
-  override def toString = new String(array, java.nio.charset.StandardCharsets.UTF_8)
+
+  override def hashCode(): Int = java.util.Arrays.hashCode(array)
+
+  override def toString = try {
+    StandardCharsets.UTF_8
+      .newDecoder()
+      .onMalformedInput(CodingErrorAction.REPORT)
+      .onUnmappableCharacter(CodingErrorAction.REPORT)
+      .decode(ByteBuffer.wrap(array))
+      .toString
+  } catch (_ =>    s"0x${new BigInteger(1, array).toString(16).toUpperCase}")
 }
